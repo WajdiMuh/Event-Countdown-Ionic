d232c97d0fc71ed8f3be52b854dbd897a0119159
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-02-23 01:18:03 +0100

    recalculate every second

--- src/app/pages/countdown/countdown.page.ts
+++ src/app/pages/countdown/countdown.page.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Event } from '../../classes/Event';
 import * as moment from 'moment';
 import { EventsService } from '../../events.service';
 import { ActivatedRoute } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.page.html',
   styleUrls: ['./countdown.page.scss'],
 })
-export class CountdownPage implements OnInit {
+export class CountdownPage implements OnInit,OnDestroy {
   finishedloading:Boolean = false;
   latestevent:Event = undefined;
   receivedevent:Event = undefined;
   daysleft: string = "";
+  subscription: Subscription;
   constructor(private eventservice:EventsService,private route: ActivatedRoute ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       if(params.id == undefined){
         this.eventservice.getLatestEvent().subscribe(result => {
+          this.subscription = interval(1000).subscribe(x => {
+              this.recalculate();
+          });
           this.latestevent = result;
           this.recalculate();
           this.finishedloading = true;
@@ -27,12 +32,19 @@ export class CountdownPage implements OnInit {
           this.finishedloading = true;
         });
       }else{
+        this.subscription = interval(1000).subscribe(x => {
+          this.recalculate();
+        });
         this.latestevent = {id:parseInt(params.id),title:params.title, date: new Date(params.date)};
         this.receivedevent = this.latestevent;
         this.recalculate();
         this.finishedloading = true;
       }
     });
+  }
+
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
   }
 
   refresh(event){
